ee08dd1c789a7e01708b01b8762763660d6f8208
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-12-03 22:58:18 +0000

    Deck class ready

--- Deck.py
+++ Deck.py
@@ -1,30 +1,31 @@
 # Deck class
 # Object that models deck of cards in the easy21 game
-
+import random
 
 class Deck():
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        # Could initialise with card value ranges and proportion of -ve and +ve cards
+        pass
 
-	def draw():
-    '''
-    Draw card. Cards have value of 1-10, uniformly distributed and color
-    black or red, 2/3 black and 1/3 red
 
-    Returns:
-        tuple. (number, colour)
-    '''
+    def draw(self   ):
+        '''
+        Draw card. Cards have value of 1-10, uniformly distributed and color
+        black or red, 2/3 black and 1/3 red
 
-	    n = random.randint(1,10)
+        Returns:
+            tuple. (number, colour)
+        '''
+        n = random.randint(1,10)
 
-	    p = random.random()
+        p = random.random()
 
-	    if (p <= 1.0/3.0):
-	        # If card is red value is -ve
-	        c = -1*n
-	    else:
-	        c = n
+        if (p <= 1.0/3.0):
+            # If card is red value is -ve
+            c = -1*n
+        else:
+            c = n
 
-	    return c
+        return c
 

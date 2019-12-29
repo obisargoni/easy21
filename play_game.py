59e59e2f1a7eed5106a528c2ece10600b01efc85
Obi Sargoni <othompsarg@hotmail.com>
2019-12-29 13:30:33 +0000

    Unpacking estimate of value function into a dataframe. Plotting value function estimate.

--- play_game.py
+++ play_game.py
@@ -4,19 +4,16 @@ import pandas as pd
 from random import random
 from Environment import Environment
 
-# State defined by agents hand (1-21) dealers hand (1-21) and agent action (True, False)
-v = np.zeros([21,21,2])
-v = {}
-n = {}
+import matplotlib as mpl
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
 
-df_value = pd.DataFrame(columns = ["state", "n", "v"])
-
-def play_game():
+def play_game(v, n):
 	'''Play an interation of the game until terminal state is reached. 
 	Use total reward received for game to update estimate of state value function.
 	'''
-	global v
-	global n
 
 	states_visited = []
 	game_reward = 0
@@ -48,7 +45,52 @@ def play_game():
 			n[s] += 1
 			v[s] += (1/float(n[s]))*(game_reward - v[s])
 
+	return v, n
 
-# Function to play many card games in order to estimate value function
-for i in range(10000):
-	play_game()
+
+def estimate_value_function(n_iters):
+	
+	v = {}
+	n = {}
+	
+	# Function to play many card games in order to estimate value function
+	for i in range(n_iters):
+		v,n = play_game(v,n)
+
+	# Now unpack value estimation of each state
+	k = v.keys()
+	#k = list(k)
+	v_list = [ list(i) + [v[i]] for i in k]
+	df_value = pd.DataFrame(columns = ['hand1', 'hand2', 'action', 'value'], data = v_list)
+
+	return df_value
+
+def plot_value_function(df_value):
+	# Plot values
+
+	# Create 2d arrays to represent all possible dealer and player hands
+	x = np.arange(1,11) * np.ones(10)[:, np.newaxis]
+	y = np.ones([10,10]) * np.arange(12,22)[:, np.newaxis]
+	z = np.zeros([10,10])
+
+	for i, r in df_value.iterrows():
+	    if r['hand1'] > 11:
+	        z[r['hand1']-12,r['hand2']-1] = r['value']
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	#ax.plot_wireframe(x, y, z, rstride=1, cstride=1)
+	surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+	plt.show()
+
+	return ax
+
+
+df_value_10k = estimate_value_function(10000)
+plot_value_function(df_value_10k)
+
+df_value_50k = estimate_value_function(50000)
+plot_value_function(df_value_50k)
+
+df_value_100k = estimate_value_function(100000)
+plot_value_function(df_value_100k)
